2665c16c2374ea6552e456c26b3a72b6d679b4c0
Imtiaz Ahmad <imtiazahmadswl93@gmail.com>
2024-07-22 21:51:43 +0500

    done with edit functionality

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const contactModel = require("./Models/contact");
 
 const app = express();
@@ -8,44 +8,67 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(
-    "mongodb+srv://nabeelsaji1:511UXWpFxl9g2GNM@cluster0.h4tfc.mongodb.net/imtiaz"
-  );
-  app.get("/get", async (req, res) => {
-    try {
-      const data = await contactModel.find();
-      return res.status(200).json({
-        data,
-        message: "Succesfully",
-      });
-    } catch (error) {
-      return res.status(500).json({ message: error.message });
-    }
-  });
+  "mongodb+srv://nabeelsaji1:511UXWpFxl9g2GNM@cluster0.h4tfc.mongodb.net/imtiaz"
+);
+app.get("/get", async (req, res) => {
+  try {
+    const data = await contactModel.find();
+    return res.status(200).json({
+      data,
+      message: "Succesfully",
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
 
+app.post("/add", async (req, res) => {
+  try {
+    const { Name, PhoneNo, Address } = req.body;
 
-  app.post("/add", async (req, res) => {  
-    try {
-      const { Name, PhoneNo, Address } = req.body;
-  
-      const data = await contactModel.create({ Name, PhoneNo, Address });
-  
-      return res.status(200).json({
-        data,
-        message: "Created Succesfully",
-      });
+    const data = await contactModel.create({ Name, PhoneNo, Address });
 
-    } catch (error) {
-      return res.status(500).json({ message: error.message });
-    }
-  });
-   
-  app.delete('/delete/:id',(req,res)=>{
-    const {id}=req.params;
-    contactModel.findByIdAndDelete({_id:id})
-    .then(result=>res.json(result))
-    .catch(err=>res.json(err))
+    return res.status(200).json({
+      data,
+      message: "Created Succesfully",
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+app.patch("/update/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { Name, PhoneNo, Address } = req.body;
+
+    const data = await contactModel.findByIdAndUpdate(id, {
+      Name,
+      PhoneNo,
+      Address,
+    });
+    return res.status(200).json({
+      data,
+      message: "Deleted Succesfully",
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = await contactModel.findByIdAndDelete({ _id: id });
+    return res.status(200).json({
+      data,
+      message: "Deleted Succesfully",
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 });
 
 app.listen(3001, () => {
-    console.log("server is runing")
-})
+  console.log("server is runing");
+});
